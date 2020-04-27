e35013f1b7171c498900f97c2d492e4e81a46608
marcinchaluda <chaluda@interia.pl>
2020-04-27 18:35:02 +0200

    update create enemy by generate random health and strength

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 
 def create_board(width, height):
@@ -31,29 +32,36 @@ def put_player_on_board(board, player):
     pass
 
 
+def create_enemies(level_one_enemies=1, level_two_enemies=1, level_three_enemies=1):
+    number_of_enemies = [level_one_enemies, level_two_enemies, level_three_enemies]
+    enemy_name = [2, 3, 4]
+    index = 1
+    enemies = {}
+    for number in range(len(number_of_enemies)):
+        name = enemy_name(number)
+        for enemy in range(number_of_enemies[number]):
+            coordinates = (0, 0) # get_random_coordinates() will be implemented
+            if name == enemy_name[0]:
+                health = random.randint(1, 15)
+                strength = random.randint(1, 15)
+            elif name == enemy_name[1]:
+                health = random.randint(15, 30)
+                strength = random.randint(15, 30)
+            else:
+                health = random.randint(30, 50)
+                strength = random.randint(30, 50)
+            enemies[f"Enemy {index}"] = create_avatar_attributes(coordinates, name, health, strength)
+            index += 1
+        name += 1
+    return enemies
+
+
 def create_avatar_attributes(coordinates, name, health_points, strength_points, avatar_type="opponent"):
     pos_X, pos_Y = coordinates
     return {"pos_X": pos_X, "pos_Y": pos_Y, "name": name, "type": avatar_type, "helath": health_points, "strength": strength_points}
 
 
-def create_enemies(level_one_enemies=1, level_two_enemies=1, level_three_enemies=1):
-    number_of_enemies = [level_one_enemies, level_two_enemies, level_three_enemies]
-    name = 2
-    index = 1
-    enemies = {}
-    for number in range(len(number_of_enemies)):
-        for enemy in range(number_of_enemies[number]):
-            coordinates = (0, 0) # get_random_coordinates() will be implemented
-            if name == 2:
-                health = 10
-                strength = 10
-            elif name == 3:
-                health = 25
-                strength = 20
-            else:
-                health = 35
-                strength = 25
-            enemies[f"ENEMY{index}"] = create_avatar_attributes(coordinates, name, health, strength)
-            index += 1
-        name += 1
-    return enemies
+def get_random_coordinates():
+    pos_X = 0
+    pos_Y = 0
+    return pos_X, pos_Y
